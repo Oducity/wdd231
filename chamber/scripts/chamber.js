d66a4d873421e251325d3a4c1d4742ac72f51f1d
Oducity <henry.odumamwen@gmail.com>
2026-01-14 17:55:28 +0100

    Created the getMembersData() async function and the displayMembers function in the chamber.js file

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -1,1 +1,23 @@
-const bList = document.querySelector("#directory-box")
+//store the .json file in a variable url.
+const url = "https://oducity.github.io/wdd231/chamber/scripts/chamber.json";
+
+//create a cards variable and store element with id="cards" in it.
+const cards = document.querySelector("#cards")
+
+// Create async function to fecth from the JSON source url using await fetch() method.
+
+async function getMemebersData() {
+    const response = await fetch(url);
+    const data = await response.json();
+    //console.table(data.prophets);   //this helpin viewing large amount of data in a table in the console.
+    displayMembers(data.members);
+};
+
+
+
+//Create a displayMembers() function to process members data.
+const displayMembers = (members) => {
+    members.forEach((member) => {
+        let card = document.createElement("section");
+    });
+}
