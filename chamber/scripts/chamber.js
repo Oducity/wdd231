162af3a55b3688f9d0a165ae874658eb96081fc7
Oducity <henry.odumamwen@gmail.com>
2026-01-14 23:44:43 +0100

    Developed the chamber.js file.

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -14,10 +14,87 @@ async function getMemebersData() {
 };
 
 
+getMemebersData();
+
 
 //Create a displayMembers() function to process members data.
 const displayMembers = (members) => {
     members.forEach((member) => {
+        let star;
+        if(member.review === 5){
+            star = `\u2605`+`\u2605`+`\u2605`+`\u2605`+`\u2605`;
+        } else if (member.review === 4){
+            star = `\u2605`+`\u2605`+`\u2605`+`\u2605`+`\u2606`;
+        } else if (member.review === 3){
+            star = `\u2605`+`\u2605`+`\u2605`+`\u2606`+`\u2606`;
+        } else if (member.review === 2) {
+            star = `\u2605`+`\u2605`+`\u2606`+`\u2606`+`\u2606`;
+        } else if (member.review === 1) {
+            star = `\u2605`+`\u2606`+`\u2606`+`\u2606`+`\u2606`;
+        } else {
+            star = "No review yet";
+        }
+
+        let level;
+        if (member.membership === 3) {
+            level = "Gold";
+        } else if (member.membership === 2) {
+            level = "Silver";
+        } else {
+            level = "Member";
+        }
+
         let card = document.createElement("section");
+
+        let logo = document.createElement("img");
+        let companyName = document.createElement("h2");
+        let addr = document.createElement("p");
+        let phoneNumber = document.createElement("p");
+        let message = document.createElement("p");
+        let statusBox = document.createElement("p");
+        let rated = document.createElement("span");
+        let memberLevel = document.createElement("span");
+        let url = document.createElement("p");
+
+        logo.setAttribute("src", member.image);
+        logo.setAttribute("alt", `Logo of ${member.companyname}`);
+        logo.setAttribute("loading", "lazy");
+        logo.setAttribute("width", "120px");
+        logo.setAttribute("heigth", "auto");
+
+        companyName.innerText = member.companyname;
+        companyName.setAttribute(".name");
+
+        addr.innerText = member.address;
+        addr.setAttribute(".address");
+
+        phoneNumber.innerHTML = `<strong>Phone:</strong> ${member.phone}`;
+        phoneNumber.setAttribute(".call-number");
+
+        message.innerText = member.intro;
+        message.setAttribute(".message");
+
+        rated.innerText = star;
+        rated.setAttribute(".rating");
+        
+        memberLevel.innerHTML = `<strong>Membership</strong>${level}`;
+        memberLevel.setAttribute(".level");
+        
+        statusBox.appendChild(memberLevel);
+        statusBox.appendChild(rated);
+        statusBox.setAttribute(".status");
+
+        url.innerHTML = `<strong>Website:</strong> <a>${member.url}`;
+        url.setAttribute(".website");
+
+        card.appendChild(logo);
+        card.appendChild(companyName);
+        card.appendChild(addr);
+        card.appendChild(phoneNumber);
+        card.appendChild(statusBox);
+        card.appendChild(url);
+
+        cards.appendChild(card);
+
     });
 }
