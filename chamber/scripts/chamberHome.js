22b5719057777487fc71861b48a256206b5d06dc
Oducity <henry.odumamwen@gmail.com>
2026-01-20 20:23:16 +0100

    Developed chamberHome.js file to dynamically generate wayfinder and display <nav> text.

---
+++ chamber/scripts/chamberHome.js
@@ -0,0 +1,40 @@
+const cards = document.querySelector("#cards");
+
+const hamburgerBtn = document.getElementById("ham-btn");
+const navBtn = document.getElementById("nav-bar");
+
+const navList = document.querySelectorAll("a");//This target and store all <a> element in navList variable
+const h1 = document.querySelector("h1");
+
+
+const homePage = document.getElementById("home-pg");
+const discoveryPage = document.getElementById("discovery-pg");
+const directoryPage = document.getElementById("directory-pg");
+const joinUsPage = document.getElementById("join-pg");
+const aboutPage = document.getElementById("about-pg");
+
+hamburgerBtn.addEventListener("click", () => {
+    hamburgerBtn.classList.toggle("show");
+    navBtn.classList.toggle("show");
+});
+
+// Code for way finder 
+addClassName(navList);
+
+function addClassName(allClass) {
+    
+    allClass.forEach(navItem => {
+        
+        navItem.addEventListener("click", () => {
+            
+            h1.innerText = navItem.innerText;
+            navItem.classList.toggle("current");
+            //This remove the className "way" from all anchor elements.
+            navList.forEach(nava => {
+                nava.classList.remove("way");
+            });
+            //This add the className "way" to the current anchor element for wayfinder use in css.
+            navItem.classList.add("way");
+        });
+    });
+}
