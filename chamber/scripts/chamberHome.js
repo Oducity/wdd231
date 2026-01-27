d598b37d045482478f201e051fbe2ea9214ba135
Oducity <henry.odumamwen@gmail.com>
2026-01-27 17:03:15 +0100

    Displayed the current weather information to the webpage.

--- chamber/scripts/chamberHome.js
+++ chamber/scripts/chamberHome.js
@@ -1,7 +1,35 @@
 const url = "https://oducity.github.io/wdd231/chamber/data/members.json";
 
+//Create required variables for the weather url.
+const myKey = "595df96ec4974ae93556a6fd7e848f50";
+const myLat = "6.33";
+const myLong = "5.62";
+
+// create the weather url
+const weatherUrl = `//api.openweathermap.org/data/2.5/weather?lat=${myLat}&lon=${myLong}&appid=${myKey}&units=metric`;
+
+/********************** Select all elements for current weather display ***********************/
+const weatherIcon = document.querySelector("#w-logo");
+const currentCity = document.querySelector("#city");
+const temp = document.querySelector("#temp");
+const myDescription = document.querySelector("#my-description");
+const highTemp = document.querySelector("#temp-high");
+const lowTemp = document.querySelector("#temp-low");
+const humid = document.querySelector("#humid");
+const sunRise = document.querySelector("#sunrise");
+const sunSet = document.querySelector("#sunset");
+
+/******************* Select all alements for more days forecast ***********************/
+const today = document.querySelector("#today");
+const dayTwo = document.querySelector("#day-two");
+const dayThree = document.querySelector("#day-three");
+const dayFour = document.querySelector("#day-four");
+const dayFive = document.querySelector("#day-five");
+
+
 const cards = document.querySelector("#cards");
 const heroBox = document.querySelector("#hero-box");
+const newsCards = document.querySelector("#news");
 
 const hamburgerBtn = document.getElementById("ham-btn");
 const navBtn = document.getElementById("nav-bar");
@@ -54,81 +82,136 @@ function displayHeroImages(heros){
     });
 };
 
-getCompaniesData();
+getApiData(url);
+getApiData(weatherUrl);
 
-async function getCompaniesData() {
-    const response = await fetch(url);
-    const data = await response.json();
-    //console.table(data.prophets);   //this helpin viewing large amount of data in a table in the console.
-    displayCompaniesData(data.companies);
+async function getApiData(arr) {
+    try {
+        const response = await fetch(arr);
+        if (response.ok) {
+                const data = await response.json();
+                console.log(data);
+                //console.table(data.prophets);   //this helpin viewing large amount of data in a table in the console.
+                if (!data.companies) {
+                        displayWeatherData(data);
+                    } else if (data.companies) {
+                        displayCompaniesData(data.companies);
+                }
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    } 
 };
 
 
 function displayCompaniesData(companies){
 
-    companies.forEach((company) => {
+    //let randomCompanies = Math.floor(Math.random() * companies.length);
+    let filteredCompanies = companies.filter((company) => company.membership > 1);
+
+    const threeCompanies = getRandomCompanies(filteredCompanies, 3)
+    threeCompanies.forEach((company) => {
+        
         let companySlot = document.createElement("section");
         companySlot.setAttribute("class", "slot");
-        if (company.rating > 1) {
 
-            let companyName = document.createElement("h2"); // fullname containner
-            companyName.setAttribute("class", "company-name")
+        let companyName = document.createElement("h2"); // fullname containner
+        companyName.setAttribute("class", "company-name")
 
-            let tag = document.createElement("p"); //Tag container
-            tag.setAttribute("class", "tag");
+        let tag = document.createElement("p"); //Tag container
+        tag.setAttribute("class", "tag");
 
-            let infoBox = document.createElement("div"); // Company information box
-            infoBox.setAttribute("class", "info-box");
+        let infoBox = document.createElement("div"); // Company information box
+        infoBox.setAttribute("class", "info-box");
 
-            let addressP = document.createElement("p"); // address box
-            addressP.setAttribute("class", "address");
+        let addressP = document.createElement("p"); // address box
+        addressP.setAttribute("class", "address");
 
-            let phoneP = document.createElement("p"); // Phone number box
-            phoneP.setAttribute("class", "phone");
+        let phoneP = document.createElement("p"); // Phone number box
+        phoneP.setAttribute("class", "phone");
 
-            let emailBox = document.createElement("p"); //Company email address containner
-            emailBox.setAttribute("class", "email");
-            emailBox.innerHTML = `<strong>Email:</strong> ${company.email}`;
-
-
-            let urlAnchor = document.createElement("a"); // Web address anchor link
-            urlAnchor.setAttribute("href", "#");
-            urlAnchor.innerText = company.url;
-            let companyUrl = document.createElement("p"); // Url anchor containner
-            companyUrl.setAttribute("class", "url");
+        let emailBox = document.createElement("p"); //Company email address containner
+        emailBox.setAttribute("class", "email");
+        emailBox.innerHTML = `<strong>Email:</strong> ${company.email}`;
 
 
-            //store company's name object in a variable "name"
-            let name = company.name;
-            companyName.innerText = name;//Paces name in the element variable "companyName"
-            companySlot.appendChild(companyName);
-
-            tag.innerText = company.companytag;
-            companySlot.appendChild(tag);
+        let urlAnchor = document.createElement("a"); // Web address anchor link
+        urlAnchor.setAttribute("href", "#");
+        urlAnchor.innerText = company.url;
+        let companyUrl = document.createElement("p"); // Url anchor containner
+        companyUrl.setAttribute("class", "url");
 
 
-            // Creates Image and its attributes
-            let portrait = document.createElement("img");
-            let imgAlt = `Photo of ${name}`;
-            portrait.setAttribute("src", company.imageurl);
-            portrait.setAttribute("alt", imgAlt);
-            portrait.setAttribute("loading", "lazy");
-            portrait.setAttribute("width", "80px");
-            portrait.setAttribute("heigth", "auto");
+        //store company's name object in a variable "name"
+        let name = company.name;
+        companyName.innerText = name;//Paces name in the element variable "companyName"
+        companySlot.appendChild(companyName);
 
-            infoBox.appendChild(portrait);
+        tag.innerText = company.companytag;
+        companySlot.appendChild(tag);
 
-            infoBox.appendChild(emailBox);
 
-            phoneP.innerHTML = `<strong>Phone:</strong> ${company.phone}`;
-            infoBox.appendChild(phoneP);
+        // Creates Image and its attributes
+        let portrait = document.createElement("img");
+        let imgAlt = `Photo of ${name}`;
+        portrait.setAttribute("src", company.imageurl);
+        portrait.setAttribute("alt", imgAlt);
+        portrait.setAttribute("loading", "lazy");
+        portrait.setAttribute("width", "80px");
+        portrait.setAttribute("heigth", "auto");
 
-            companyUrl.appendChild(urlAnchor);
-            infoBox.appendChild(companyUrl);
+        infoBox.appendChild(portrait);
 
-            companySlot.appendChild(infoBox);
+        infoBox.appendChild(emailBox);
 
-        };
+        phoneP.innerHTML = `<strong>Phone:</strong> ${company.phone}`;
+        infoBox.appendChild(phoneP);
+
+        companyUrl.appendChild(urlAnchor);
+        infoBox.appendChild(companyUrl);
+
+        companySlot.appendChild(infoBox);
+
         cards.appendChild(companySlot);
+    
     });
+};
+
+
+function getRandomCompanies(arr, count) {
+    const compArrey = arr;
+    const result = [];
+    for (let i = 0; i < count; i++) {
+        const randomIndex = Math.floor(Math.random() * compArrey.length);
+
+        const selectedIndex = compArrey.splice(randomIndex);
+        result.push(selectedIndex);
+    }
+    return result;
+}
+
+
+/********************** news and weather ************************/
+
+
+function displayWeatherData(data) {
+    console.log("Hello");
+
+    currentCity.innerText = data.name;
+    let iconAddress = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+    weatherIcon.setAttribute("src", iconAddress);
+    weatherIcon.setAttribute("alt", data.weather[0].description);
+    temp.innerHTML = `<strong>${data.main.temp}&degC`;
+    myDescription.innerText = data.weather[0].description;
+    highTemp.innerHTML = `High: ${data.main.temp_max}&degC`;
+    lowTemp.innerHTML = `Low: ${data.main.temp_min}&degC`;
+    humid.innerHTML = `${data.main.humidity}%`;
+    sunRise.innerHTML = data.sys.sunrise;
+    sunSet.innerHTML = data.sys.sunset;
+};
+
+function displayDaysData(data) {
+
 };
