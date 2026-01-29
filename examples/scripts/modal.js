963703bea7f868258fa3fc42102a6aa4520c7cfd
Oducity <henry.odumamwen@gmail.com>
2026-01-29 14:10:56 +0100

    Developed modal.css and modal.js files and linked them within the head elements.

---
+++ examples/scripts/modal.js
@@ -0,0 +1,8 @@
+const modal = document.querySelector("#myModal");
+const closeModal = document.querySelector("#closeModal");
+
+modal.show();
+
+closeModal.addEventListener("click", () => {
+    modal.closest();
+})
