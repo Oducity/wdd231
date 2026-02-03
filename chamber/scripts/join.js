d01d1da486892c6da381cc260793aa8553324bc8
Oducity <henry.odumamwen@gmail.com>
2026-02-04 00:19:58 +0100

    Created 4 eventListeners to pass each object membership level details and benefits to the DOM.

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -1,7 +1,7 @@
 
 const cards = document.querySelector("#cards-join");
-
 const time = document.querySelector("#time");
+const modalBox = document.querySelector("#modal-box");
 
 computeTimeStamp();
 
@@ -9,6 +9,8 @@ function computeTimeStamp () {
     const currentDate = new Date();
     time.setAttribute("datetime", currentDate);
 }
+
+
 
 createMembershipCard();
 
@@ -22,6 +24,9 @@ function createMembershipCard () {
     const nonprofitH2 = document.createElement("h3");
     nonprofitH2.innerText = "Non Profit Membership Level";
     const nonButton = document.createElement("button");
+    nonButton.setAttribute("type", "button");
+    nonButton.setAttribute("id", "nonbutton");
+    nonButton.setAttribute("class", "nonbutton")    
     nonButton.innerText = "Learn more";
     nonProfitCard.appendChild(nonprofitH2);
     nonProfitCard.appendChild(nonButton);
@@ -32,6 +37,9 @@ function createMembershipCard () {
     const bronzeCardH2 = document.createElement("h3")
     bronzeCardH2.innerText = "Bronze Membership Level";
     const bronzeButton = document.createElement("button");
+    bronzeButton.setAttribute("type", "button");
+    bronzeButton.setAttribute("id", "bronzebutton");
+    bronzeButton.setAttribute("class", "bronzebutton");
     bronzeButton.innerText = "Learn more";
     bronzeCard.appendChild(bronzeCardH2);
     bronzeCard.appendChild(bronzeButton);
@@ -41,6 +49,9 @@ function createMembershipCard () {
     const silverCardH2 = document.createElement("h3");
     silverCardH2.innerText = "Silver Membership Level";
     const silverButton = document.createElement("button");
+    silverButton.setAttribute("type", "button");
+    silverButton.setAttribute("id", "silverbutton");
+    silverButton.setAttribute("class", "silverbutton");
     silverButton.innerText = "Learn more";
     silverCard.appendChild(silverCardH2);
     silverCard.appendChild(silverButton);
@@ -50,6 +61,9 @@ function createMembershipCard () {
     const goldCardH2 = document.createElement("h3");
     goldCardH2.innerText = "Gold Membership Level";
     const goldButton = document.createElement("button");
+    goldButton.setAttribute("type", "button");
+    goldButton.setAttribute("id", "goldbutton");
+    goldButton.setAttribute("class", "goldbutton");
     goldButton.innerText = "Learn more";
     goldCard.appendChild(goldCardH2);
     goldCard.appendChild(goldButton);
@@ -60,22 +74,56 @@ function createMembershipCard () {
     cards.appendChild(goldCard);
 };
 
+const npBtn = document.querySelector("#nonbutton");
+const bronzeBtn = document.querySelector("#bronzebutton");
+const silverBtn = document.querySelector("#silverbutton");
+const goldBtn = document.querySelector("#goldbutton");
+
 const details = {
     np : [
-        {detail: "This is a free membership level for non profit organizations"},
-        {benefits : "Free access to clients, 5gb of cloud space"}
+        {detail: "This is a free membership level for non profit organizations", benefits : "Free access to clients, 5gb of cloud space"}
     ],
     bronze : [
-        {detail: "Bronze level is a $100 paid annual membership level"},
-        {benefits: "One dedicated email addresses, access to 30 clients monthly, annual 20gb of cloud storage, five advert placement monthly, access to ten trainning materials"}
+        {detail: "Bronze level is a $100 paid annual membership level", benefits: "One dedicated email addresses, access to 30 clients monthly, annual 20gb of cloud storage, five advert placement monthly, access to ten trainning materials"}
     ],
     silver : [
-        {detail : "Silver level is a $300 paid annual membership level"},
-        {benefits : "Three dedicated email addresses, unlimited access to clients, annual 50gb of cloud storage, fifteen advert placement monthly, access to 25 trainning materials annualy"}
+        {detail : "Silver level is a $300 paid annual membership level", benefits : "Three dedicated email addresses, unlimited access to clients, annual 50gb of cloud storage, fifteen advert placement monthly, access to 25 trainning materials annualy"}
     ],
     gold : [
-        {detail : "Gold level is a $500 paid annual membership level"},
-        {benefits : "Seven dedicated email addresses, unlimited access to clients, 150gb of cloud storage, thirty advert placement monthly, unlimited access to over 80 trainning library materials annualy, a dedicated business expert advisor"}
+        {detail : "Gold level is a $500 paid annual membership level", benefits : "Seven dedicated email addresses, unlimited access to clients, 150gb of cloud storage, thirty advert placement monthly, unlimited access to over 80 trainning library materials annualy, a dedicated business expert advisor"}
     ]       
 };
 
+npBtn.addEventListener("click", () => {
+    displayLevelDetails(details.np);
+});
+
+bronzeBtn.addEventListener("click", () => {
+    displayLevelDetails(details.bronze);
+});
+
+silverBtn.addEventListener("click", () => {
+    displayLevelDetails(details.silver);
+});
+
+goldBtn.addEventListener("click", () => {
+    displayLevelDetails(details.gold);
+});
+
+function displayLevelDetails (levelData) {
+    modalBox.innerHTML = "";
+    levelData.forEach (data => {
+        let pDetails = document.createElement("p");
+        pDetails.setAttribute("class", "details-p");
+
+        let pBenefits = document.createElement("p");
+        pBenefits.setAttribute("class", "benefits-p");
+
+        pDetails.innerHTML = `<strong>Details:</strong> ${data.detail}`;
+        pBenefits.innerHTML = `<strong>Benefits:</strong> ${data.benefits}`;
+
+        modalBox.appendChild(pDetails);
+        modalBox.appendChild(pBenefits);
+        
+    });
+};
