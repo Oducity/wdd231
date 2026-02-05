c92d833e8fa2983e16d55efd6d5c44f87a1e67b6
Oducity <henry.odumamwen@gmail.com>
2026-02-05 15:01:31 +0100

    Removed leabel element from the time element in the join.html file.

--- chamber/scripts/thank-you.js
+++ chamber/scripts/thank-you.js
@@ -1,14 +1,39 @@
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 
-const firstName = urlParams.get(`first-name`);
-const lastName = urlParams.get(`last-name`);
-const email = urlParams.get(`email`);
-const phone = urlParams.get(`phone`);
-const businessTitle = urlParams.get(`business-title`);
-const businessName = urlParams.get(`business-name`);
-const membershipLevel = urlParams.get(`membership-level`);
-const description = urlParams.get(`description`);
-const dateTime = urlParams.get(`time`);
-console.log(urlParams);
-console.log(dateTime);
+const thanksBox = document.querySelector("#thanks-box");
+
+desplayFormData(urlParams);
+
+function desplayFormData (data) {
+    const appreciation = document.createElement("p");
+    const dataBox = document.createElement("div");
+
+    const instruction = document.createElement("p");
+    instruction.innerText = `Your registration information is as follows:`;
+    const firstName = data.get(`first-name`);
+    const lastName = data.get(`last-name`);
+    const email = data.get(`email`);
+    const phone = data.get(`phone`);
+    const businessTitle = data.get(`business-title`);
+    const businessName = data.get(`business-name`);
+    const membershipLevel = data.get(`membership-level`);
+    const description = data.get(`description`);
+
+    appreciation.innerText = `Thank you ${firstName} ${lastName} for registring your company "${businessName}" with us.`;
+    thanksBox.appendChild(appreciation);
+
+    dataBox.appendChild(instruction);
+    dataBox.appendChild(`span>First Name</strong>: <span>${firstName}</span>`);
+    dataBox.appendChild(`span>Last Name</strong>: <span>${lastName}</span>`);
+    dataBox.appendChild(`span>Email</strong>: <span>${email}</span>`);
+    dataBox.appendChild(`span>Phone</strong>: <span>${phone}</span>`);
+    dataBox.appendChild(`span>Business Title</strong>: <span>${businessTitle}</span>`);
+    dataBox.appendChild(`span>Business Name</strong>: <span>${businessName}</span>`);
+    dataBox.appendChild(`span>Membership</strong>: <span>${membershipLevel}</span>`);
+    dataBox.appendChild(`span>Description</strong>: <span>${description}</span>`);
+
+    thanksBox.appendChild(thanksBox);
+
+}
+
