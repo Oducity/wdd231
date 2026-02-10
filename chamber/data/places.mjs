d99869f7993470f25cdb20250757242547c20cd0
Oducity <henry.odumamwen@gmail.com>
2026-02-10 01:10:39 +0100

    Changed and optimized oba-palace.webp file to improve lcp.

--- chamber/data/places.mjs
+++ chamber/data/places.mjs
@@ -2,7 +2,7 @@
     {
       "name": "Oba Palace",
       "address": "Specific Identifier: 8JJC+R4G, Benin City 300102",
-      "imgurl": "../chamber/images/oba-royal-palace.webp",
+      "imgurl": "../chamber/images/oba-palace.webp",
       "description": "A unisco listed site and the residence of the traditional ruler of the Benin Kingdom. It is one of the most respected historical monuments in Nigeria, though visitors must adhere to strict native protocols. As the official residence of the traditional ruler, this site benefits from dedicated traditional and state security personnel who enforce strict entry and conduct protocols, focusing heavily on the safety and sanctity of the premises.",
       "type": "traditional-grand"
     },
